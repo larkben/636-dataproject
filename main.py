75ce91d7f7070f37ed75ca497574c8eefd1bba5e
AidanHenrichs <112669975+AidanHenrichs@users.noreply.github.com>
2024-10-30 13:12:59 -0500

    Update main.py
    starting to write the function that will allow users to say what company they want and then get the x information regarding it

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from bs4 import BeautifulSoup
+import requests
 import pandas as pd
 
-print("Hello World!")
+def get_url(company, date_start=None, date_end=None):
+    print()
+
+if __name__ == '__main__':
+    req = requests.get('https://x.com/DividendTalks/status/1850279953075675313')
+    soup = BeautifulSoup(req.text, features='html.parser')
+    print(soup.prettify())
